e5ef1e3fd22a92716275078961d30c39d8f26529
Cheuk-ting Chan <cheuktingchan@outlook.com>
2021-04-08 21:49:47 +0100

    Hexagon grid square plan

--- main/sketch.js
+++ main/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  // put setup code here
+  createCanvas(800, 800);
+  size = 20;
+  w = size * Math.sqrt(3);
+  h = size * 2
+  canvasdiag = 20;
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+  for (i = 0; i < canvasdiag; i++){
+    for (j = 0; j <canvasdiag; j++){
+      if (j % 2 == 0){
+        rect(i * w, j * h*0.75, w, h);
+        fill('rgba(0,255,0, 0.1)')
+          }else{
+            rect((0.5 * w) + i * w, j * h*0.75, w, h);
+            fill('rgba(0,255,0, 0.1)')
+          }
+    }
+  }
 }
